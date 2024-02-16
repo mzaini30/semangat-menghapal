bc8f108f7916e1042a59cb757167a02c3c696b65
mzaini30 <muhzaini30@gmail.com>
2024-02-16 23:01:33 +0800

    simpan dulu

--- src/App.svelte
+++ src/App.svelte
@@ -3,9 +3,30 @@
   import Gerbang from "./kunci/src/Gerbang.svelte";
   import data from "../android.json";
   import Beranda from "./Beranda.svelte";
+  import alasql from "alasql";
+
+  alasql(/* sql */ `create localStorage database if not exists data;
+  attach localStorage database data`);
+  alasql(
+    /* sql */ `CREATE TABLE IF NOT EXISTS data.hapalan (id text, hapalan INTEGER)`
+  );
+  alasql(
+    /* sql */ `INSERT INTO data.hapalan VALUES ('${crypto.randomUUID()}', 0)`
+  );
+
+  if (localStorage.database) {
+    let id = alasql(/* sql */ `select id from data.hapalan `)[0].id;
+
+    let hapalanDatabaseVersiLawas = JSON.parse(localStorage.database).hapalan[0]
+      .hapalan;
+
+    alasql(
+      /* sql */ `update data.hapalan set hapalan = ${hapalanDatabaseVersiLawas} where id = '${id}' `
+    );
+  }
 
   const routes = {
-    '/': Beranda
+    "/": Beranda,
   };
 </script>
 
